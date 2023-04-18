54cf03af9a7665d4eb3c6b821ab7cccbbe1546c3
fujidaiti <fujidaiti@gmail.com>
2023-04-18 19:44:31 +0900

    hide some classes annotated with  to fix static analysis issue

--- package/lib/exprollable_page_view.dart
+++ package/lib/exprollable_page_view.dart
@@ -1,4 +1,8 @@
 library exprollable_page_view;
 
 export 'package:exprollable_page_view/src/addon/addon.dart';
-export 'package:exprollable_page_view/src/core/core.dart';
+export 'package:exprollable_page_view/src/core/core.dart'
+    hide
+        InheritedExprollablePageController,
+        InheritedPageContentScrollController,
+        InheritedViewportController;
