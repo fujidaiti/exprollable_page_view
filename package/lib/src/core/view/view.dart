26c2ce4d21fd1b9bbf4d84434a6460b8e74b8fd9
fujidaiti <fujidaiti@gmail.com>
2023-06-13 19:29:20 +0900

    add new stuffs to public API

--- package/lib/src/core/view/view.dart
+++ package/lib/src/core/view/view.dart
@@ -1,3 +1,5 @@
 export 'package:exprollable_page_view/src/core/view/exprollable_page_view.dart';
 export 'package:exprollable_page_view/src/core/view/page_configuration.dart'
     hide InheritedPageController;
+export 'package:exprollable_page_view/src/core/view/default_page_controller.dart'
+    hide InheritedDefaultPageConfiguration;
